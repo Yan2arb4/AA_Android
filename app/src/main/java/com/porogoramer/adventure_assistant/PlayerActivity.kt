01e6596636683d5352647008fb1d8aba7edb4179
Yan2arb4 <yan2arb6@hotmail.com>
2024-10-18 17:54:48 -0400

    setup: Add hamburger menu
    Hamburger menu skeleton implemented.

--- app/src/main/java/com/porogoramer/adventure_assistant/PlayerActivity.kt
+++ app/src/main/java/com/porogoramer/adventure_assistant/PlayerActivity.kt
@@ -1,8 +1,12 @@
 package com.porogoramer.adventure_assistant
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.porogoramer.adventure_assistant.databinding.ActivityPlayerBinding
+
 
 private lateinit var binding : ActivityPlayerBinding
 
@@ -11,5 +15,19 @@ class PlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val nv = binding.navView
+        val drawer = binding.drawerLayout
+        nv.setNavigationItemSelectedListener {
+            Toast.makeText(this, "I AM REAL", Toast.LENGTH_SHORT).show()
+            drawer.closeDrawers()
+            false
+        }
+
+
+        binding.menuBtn.setOnClickListener{
+            val drawer = binding.drawerLayout
+            drawer.openDrawer(GravityCompat.START)
+        }
     }
 }
