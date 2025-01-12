2aae6a15fbc74f58cc5c2267f07813b7a89d3474
Yan2arb4 <yan2arb6@hotmail.com>
2025-01-12 15:26:10 -0500

    feat(backend) : Add basic DTOs and slight change in User class
    Remove properties in User class since it inherits all of those from IdendityUser.

--- AA_Backend/Models/User.cs
+++ AA_Backend/Models/User.cs
@@ -5,9 +5,6 @@ namespace AA_Backend.Models
 {
     public class User : IdentityUser
     {
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; } = null!;
-        public List<Character> Characters { get; set; } = null!;
+        public virtual List<Character>? Characters { get; set; } = null!;
     }
 }
